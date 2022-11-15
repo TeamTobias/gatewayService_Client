2e85d56d7435b4a78444733ec00f83a7c0d2a5c4
DietCoding <jaejoodo1@gmail.com>
2022-11-15 21:55:59 +0900

    init commit

---
+++ src/main/java/com/example/gatewayservice_client/GatewayServiceClientApplication.java
@@ -0,0 +1,15 @@
+package com.example.gatewayservice_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayServiceClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gatewayservice_client/GatewayServiceClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gatewayservice_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
